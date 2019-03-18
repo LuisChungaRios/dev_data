41d0dbced07069f893fe80cf970b2f5c47185dd0
Gianella Vargas <gianella.vq.copernico@gmail.com>
2019-03-18 10:50:04 -0600

    Función filtrado por año

--- src/data.js
+++ src/data.js
@@ -3,8 +3,13 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = () => {
+//   return 'example';
+// };
 
-window.example = example;
+// window.example = example;
+var newArray = INJURIES.filter(function (obj) {
+  return(parseInt(obj.Year) === 1960);
+  
+});
+console.log(newArray);
