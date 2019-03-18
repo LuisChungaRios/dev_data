0b6bfb9f9e23003f5e855bff45c31b7696c5ac6f
FJAsayag <Asayag2018@hotmail.com>
2019-03-18 11:31:04 -0500

    select and options created, getting select value function created on main.js

--- src/data.js
+++ src/data.js
@@ -1,10 +1,8 @@
 /* Manejo de data */
-
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
-
-window.example = example;
+// const example = () => {
+//   return 'example';
+// };
+// window.example = example;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 /* Manejo del DOM */
+
+const yearSelector = document.getElementById("yearSelect");
+let yearSelected = yearSelector.value;
+
+yearSelector.addEventListener("click", () => {
+    yearSelected = yearSelector.value;
+    console.log(yearSelected)
+
+    arrYears.indexOf(yearSelected);
+    console.log()
+}); 
